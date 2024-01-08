395e2627b9ba3fa088ef029db5018dad77aa0721
Chamara Keragala <54570293+chamarakera@users.noreply.github.com>
2024-01-08 11:19:06 +1100

    add ruff formatting action (#5)

--- src/migrator/__main__.py
+++ src/migrator/__main__.py
@@ -20,7 +20,7 @@ def main():
     deployment.scale_to_zero(cas_deployment)
 
     # select two ec2 instances from a single multi-AZ node group to retain
-    node_group = NodeGroup(config.node_group()[0]['name'])
+    node_group = NodeGroup(config.node_group()[0]["name"])
     single_multi_az_ng = node_group.single_multi_az_node_group()
     pprint(node_group.select_instances(single_multi_az_ng))
 
